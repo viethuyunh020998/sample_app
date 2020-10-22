932a9d584bb77e9687b22abb8a50016d84001596
viethuynh <huynhquocviet020998@gmail.com>
2020-10-22 13:26:31 +0700

    Chapter 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  root "application#hello"
+  scope "(:locale)", locale: /en|vi/ do
+    get "static_pages/home", to: "static_pages#home"
+    get "static_pages/help", to: "static_pages#help"
+    root "static_pages#home"
+  end
 end
