141422f8c2ab6fb00f1940d779b2e981a226b265
viethuynh <huynhquocviet020998@gmail.com>
2020-10-23 10:56:15 +0700

    chapter-9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   before_save{email.downcase!}
-  validates :name, presence: true, length: {maximum: Settings.user.maximum_name}
+  validates :name, presence: true,
+    length: {maximum: Settings.user.maximum_name}
   validates :email, presence: true,
     length: {maximum: Settings.user.maximum_email},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: true
@@ -16,5 +19,22 @@ class User < ApplicationRecord
       BCrypt::Engine.cost
     end
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_column :remember_digest, nil
   end
 end
