3581a302fae94e2817ac139ae4258737108724d6
viethuynh <huynhquocviet020998@gmail.com>
2020-10-23 10:56:15 +0700

    chapter-7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = t "users_control.warning"
+    redirect_to root_path
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      log_in @user
+      flash[:success] = t "users_control.create"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit(:name, :email, :password, :password_confirmation)
+  end
 end
