44b13ebeab76e6f781546de09ef039481ce144f3
viethuynh <huynhquocviet020998@gmail.com>
2020-10-23 10:56:15 +0700

    chapter-12

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
     delete "/logout", to: "sessions#destroy"
     resources :users
     resources :account_activations, only: [:edit]
+    resources :password_resets,  except: [:destroy, :index, :show]
   end
 end
